b50e5ceb2a06790db33efedfe3c09bd2f3d2b728
delphox60 <delphox60@gmail.com>
2022-08-25 19:17:14 +0900

    feat: 1&2 region depth code 리스트 불러오기 기능 구현

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pymysql
 import settings
 
+region_12depth_code_list = []
+region_123depth_code_list = []
+
 db = pymysql.connect(host=settings.DB_HOST,
                     user=settings.DB_USER_NAME,
                     password=settings.DB_PASSWORD,
@@ -11,4 +14,14 @@ db = pymysql.connect(host=settings.DB_HOST,
 cursor = db.cursor(pymysql.cursors.DictCursor)
 
 cursor.execute("select region_2depth_code from address_info where region_1depth_code = 11;")
-cursor.fetchall()
+
+region_2depth_code_list = cursor.fetchall()
+for i in region_2depth_code_list:
+    
+    if (i["region_2depth_code"] == None):
+        break
+    
+    region_12depth_code_list.append("11" + str(i["region_2depth_code"]))
+    
+
+
