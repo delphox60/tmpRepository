bb5a1478e5ec506e4860671c4ccfc30a9229069c
delphox60 <delphox60@gmail.com>
2022-08-25 19:26:39 +0900

    feat: 1&2&3 region depth code 리스트 불러오기 기능 구현

--- main.py
+++ main.py
@@ -18,10 +18,16 @@ cursor.execute("select region_2depth_code from address_info where region_1depth_
 region_2depth_code_list = cursor.fetchall()
 for i in region_2depth_code_list:
     
-    if (i["region_2depth_code"] == None):
-        break
-    
     region_12depth_code_list.append("11" + str(i["region_2depth_code"]))
     
+region_12depth_code_list = list(set(region_12depth_code_list))
 
+for i in region_12depth_code_list:
+    cursor.execute("select region_3depth_code from address_info where region_1depth_code = 11 and region_2depth_code = \"%s\";" % i[2:])
+    tmp_list = cursor.fetchall()
+    for three_dc in tmp_list:
+        
+        region_123depth_code_list.append(i + str(three_dc["region_3depth_code"]))
 
+for i in region_123depth_code_list:
+    print(i)
